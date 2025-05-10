74de0c1d711a9d8ec9cf5ae9368299a919490599
Sameer Khan <101021315+sameerkhansf@users.noreply.github.com>
2025-05-09 22:18:00 -0700

    fix: correct SpeedInsights import path to use /next

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import App from "./App";
 import "./index.css";
 
